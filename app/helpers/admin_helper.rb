29b9fb6ca44c3f4714fae6ff0219035d78ba01db
imakewebthings <caleb@imakewebthings.com>
2014-07-18 09:24:06 -0700

    Move hierarchy from helper tag to recursive partial

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,18 +1,2 @@
 module AdminHelper
-  def hierarchy_list(hash)
-    hash.map do |item, children|
-      name = content_tag(:div, content_tag(:div, item[:name], class: 'name'), class: 'small-8 columns')
-      actions = content_tag :div, class: 'small-4 columns actions' do
-        concat link_to(t('edit'), url_for([:admin, item, action: :edit]), class:'tiny radius pill button')
-        concat link_to(t('delete'), url_for([:admin, item]), class:'tiny radius alert pill button', method: :delete,
-                      data: { confirm: 'Are you sure you want to delete this?'})
-      end
-      content_tag(:div, class: 'row') do
-        concat name + actions
-        if children.size > 0
-          concat content_tag(:div, hierarchy_list(children), class:'small-12 columns')
-        end
-      end
-    end.join.html_safe
-  end
 end
