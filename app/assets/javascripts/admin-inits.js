a7d29a6c486f30a7e63f0e64007c4806dd96bb27
imakewebthings <caleb@imakewebthings.com>
2014-08-29 15:11:14 -0700

    Add RTL/LTR to wysiwyg

--- app/assets/javascripts/admin-inits.js
+++ app/assets/javascripts/admin-inits.js
@@ -5,10 +5,11 @@ $document.on('page:change', function() {
   $('select[multiple]').chosen()
   tinymce.init({
     selector: 'textarea.wysiwyg',
-    toolbar: 'formatselect | bold italic underline strikethrough | bullist numlist blockquote | link unlink | table | undo redo',
-    plugins: 'link table',
+    toolbar: 'formatselect | bold italic underline strikethrough | bullist numlist blockquote | link unlink | table | undo redo | ltr rtl',
+    plugins: 'link table paste directionality',
     menubar: false,
     statusbar: false,
+    paste_remove_styles: true,
     height: 250
   })
 })
