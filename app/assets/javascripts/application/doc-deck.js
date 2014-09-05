d28d28a9dc7833acb47dfcc9389f7716c0c61933
imakewebthings <caleb@imakewebthings.com>
2014-09-04 22:45:18 -0700

    Style openseadragon for docs

--- app/assets/javascripts/application/doc-deck.js
+++ app/assets/javascripts/application/doc-deck.js
@@ -1,32 +1,63 @@
 var $document = $(document)
+var viewerCount = 0;
+
+function extractPageImages($viewer) {
+  return $.map($viewer.find('.image').get(), function(img) {
+    var $img = $(img)
+    return {
+      type: 'legacy-image-pyramid',
+      levels: [{
+        url: $img.data('src'),
+        width: parseInt($img.data('width'), 10),
+        height: parseInt($img.data('height'))
+      }]
+    }
+  })
+}
+
+function createDragon($viewer, pages) {
+  return new OpenSeadragon({
+    element: $viewer[0],
+    hash: 'viewer' + viewerCount++,
+    tileSources: pages,
+    showHomeControl: false,
+    prefixUrl: '/assets/',
+    zoomInButton: 'js-zoomin-link',
+    zoomOutButton: 'js-zoomout-link',
+    fullPageButton: 'js-fullscreen-link',
+    previousButton: 'js-prev-link',
+    nextButton: 'js-next-link',
+    navigationControlAnchor: OpenSeadragon.ControlAnchor.TOP_RIGHT,
+    sequenceControlAnchor: OpenSeadragon.ControlAnchor.BOTTOM_LEFT,
+    navigatorSizeRatio: 0.1
+  })
+}
 
 $document.on('page:change', function() {
-  if ($('.deck-container').length) {
-    $.deck()
-  }
-})
-
-$document.on('deck.change', function(event, from, to) {
-  var $viewer = $.deck('getSlide', to).find('.image-viewer')
+  var $viewer = $('.image-viewer')
   var dragon = $viewer.data('dragon')
-  var width, height
+  var pages, width, height, $texts
 
   if (!dragon) {
-    width = parseInt($viewer.data('width'), 10)
-    height = parseInt($viewer.data('height'), 10)
-    url = $viewer.data('src')
-    dragon = new OpenSeadragon({
-      element: $viewer[0],
-      hash: 'viewer' + to,
-      tileSources: {
-        type: 'legacy-image-pyramid',
-        levels: [{
-          url: url,
-          width: width,
-          height: height
-        }]
+    pages = extractPageImages($viewer)
+    dragon = createDragon($viewer, pages)
+    $viewer.data('dragon', dragon)
+    dragon.innerTracker.scrollHandler = false
+    $('.controls .total').text(pages.length)
+    $texts = $viewer.closest('.image-viewer-container').find('.page-text')
+    $texts.eq(0).addClass('active')
+    dragon.addHandler('page', function(info) {
+      $('.status .current').text(info.page + 1)
+      $texts.filter('.active').removeClass('active')
+      $texts.eq(info.page).addClass('active')
+    })
+    dragon.addHandler('pre-full-screen', function(info) {
+      if (info.fullScreen) {
+        $('.image-viewer-container .controls').appendTo('.image-viewer')
       }
     })
-    $viewer.data('dragon', dragon)
+    dragon.addHandler('full-screen', function(info) {
+      $('.image-viewer .controls').appendTo('.image-viewer-container')
+    })
   }
 })
