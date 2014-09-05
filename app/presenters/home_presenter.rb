02806fd5f70e21b25f0ffb6d3d6420bea8af18e1
imakewebthings <caleb@imakewebthings.com>
2014-09-04 22:53:00 -0700

    Use published scope

--- app/presenters/home_presenter.rb
+++ app/presenters/home_presenter.rb
@@ -2,8 +2,8 @@ class HomePresenter
   attr_reader :recent, :popular, :featured, :collaborators
 
   def initialize
-    @recent = Document.limit(3).map {|d| DocumentPresenter.new d }
-    @popular = Document.order('popular_count DESC').limit(3).map do |d|
+    @recent = Document.published.limit(3).map {|d| DocumentPresenter.new d }
+    @popular = Document.published.order('popular_count DESC').limit(3).map do |d|
       DocumentPresenter.new d
     end
     @featured = Document.featured.map {|d| DocumentPresenter.new d }
