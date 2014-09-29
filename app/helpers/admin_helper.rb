fcba32c884cf002b39c365fe355618826dd90882
imakewebthings <caleb@imakewebthings.com>
2014-09-29 11:53:23 -0700

    Document view button in admin table

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,8 +1,4 @@
 module AdminHelper
-  def item_processed?(item)
-    defined?(item.processed) && item.processed
-  end
-
   def admin_title(page_title)
     [page_title, 'SHARIAsource Admin'].flatten.reject(&:empty?).join ' | '
   end
