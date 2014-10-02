0e5be0f785c2f53be1fc6931cba2feb650e56164
imakewebthings <caleb@imakewebthings.com>
2014-10-01 21:42:49 -0700

    Author search results

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -5,5 +5,16 @@ class AuthorsController < ApplicationController
 
   def show
     @author = User.find params[:id]
+    @filters = SearchFilters.new author: [params[:id]]
+    @languages = Language.all
+    @authors = User.all
+    @topics = Topic.all
+    @themes = Theme.all
+    @regions = Region.hash_tree
+    @eras = Era.hash_tree
+    @document_types = DocumentType.hash_tree
+    @search = Document.search do |query|
+      query.with(:contributor_id, @filters.author)
+    end
   end
 end
