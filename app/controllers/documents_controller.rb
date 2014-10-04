84b2f48736dbe5e873c35889f4e10212d5d6c707
imakewebthings <caleb@imakewebthings.com>
2014-10-03 17:54:32 -0700

    Allow document owners to view unpublished documents

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,8 +1,12 @@
 class DocumentsController < ApplicationController
   def show
-    document = Document.published.find params[:id]
-    PopularityWorker.perform_async(document.id, 'increment!')
-    PopularityWorker.perform_in(3.months, document.id, 'decrement!')
+    document = Document.find params[:id]
+    if document.published
+      PopularityWorker.perform_async(document.id, 'increment!')
+      PopularityWorker.perform_in(3.months, document.id, 'decrement!')
+    elsif !document.contributor.self_and_ancestors.include?(current_user)
+      raise ActiveRecord::RecordNotFound
+    end
     @document = DocumentPresenter.new document
   end
 end
