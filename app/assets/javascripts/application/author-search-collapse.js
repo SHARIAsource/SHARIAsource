b952576070cb7000f89112f60f812d1eaa0d82a6
imakewebthings <caleb@imakewebthings.com>
2014-10-07 16:55:09 -0700

    Start search result filters in collapsed state

--- app/assets/javascripts/application/author-search-collapse.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  $(document).on('page:change', function() {
-    $('.author-search .filter-block')
-    .not('.filter-author .filter-block')
-    .addClass('is-contracted')
-    .find('.filter-content')
-    .slideUp(0)
-  })
-}())
