482f7a99d71a2823e64d35bf483470c24aa182f5
imakewebthings <caleb@imakewebthings.com>
2014-10-07 17:52:15 -0700

    Expand contract all search filters

--- app/assets/javascripts/application/filter-expand-contract.js
+++ app/assets/javascripts/application/filter-expand-contract.js
@@ -1,10 +1,28 @@
 (function() {
   $(document).on('click', '.filter-block .expand-contract', function(event) {
     var $block = $(this).closest('.filter-block')
+    var $filterHeader = $('.filter-header')
     $block.toggleClass('is-contracted')
     $block.find('.filter-content').slideToggle(300, function() {
       $(document).trigger('sameheight:refresh')
     })
+    $filterHeader.removeClass('all-expanded all-contracted')
+    if ($('.filter-block').length === $('.is-contracted').length) {
+      $filterHeader.addClass('all-contracted')
+    }
+    if (!$('.filter-block.is-contracted').length) {
+      $filterHeader.addClass('all-expanded')
+    }
+    event.preventDefault()
+  }).on('click', '.filter-header .expand-all a', function(event) {
+    var $this = $(this)
+    $('.filter-header').addClass('all-expanded')
+    $('.filter-block.is-contracted .expand-contract').trigger('click')
+    event.preventDefault()
+  }).on('click', '.filter-header .contract-all a', function(event) {
+    var $this = $(this)
+    $('.filter-header').addClass('all-contracted')
+    $('.filter-block:not(.is-contracted) .expand-contract').trigger('click')
     event.preventDefault()
   })
 }())
