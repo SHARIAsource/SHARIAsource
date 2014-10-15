0829f6ba1c1e2ef5237348143975ea71bae7d0bc
imakewebthings <caleb@imakewebthings.com>
2014-10-15 15:37:11 -0700

    Document twitter share

--- app/helpers/document_helper.rb
+++ app/helpers/document_helper.rb
@@ -6,7 +6,30 @@ module DocumentHelper
       '&body=',
       CGI.escape("#{document.title} by #{document.author_or_contributor}"),
       CGI.escape("\n\n"),
-      CGI.escape("#{document_url(document.object)}")
+      CGI.escape(document_url(document.object))
+    ].join('')
+  end
+
+  def document_twitter_share(document)
+    author = document.author_or_contributor
+    url = CGI.escape(document_url(document.object))
+    title_length_max = 120 - author.length - url.length
+    shortened_title = document.title
+    if document.title.length > title_length_max
+      shortened_title = document.title.slice(0, title_length_max)
+      if shortened_title.slice(-1) == " "
+        shortened_title = shortened_title.trim
+      else
+        shortened_title = shortened_title.split(' ')
+        shortened_title.pop
+        shortened_title = shortened_title.join(' ')
+      end
+      shortened_title += '…'
+    end
+    [
+      "https://twitter.com/share?url=#{url}",
+      '&via=SHARIAsource&text=',
+      CGI.escape("#{shortened_title} by #{author}")
     ].join('')
   end
 end
