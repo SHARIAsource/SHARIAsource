d8f396a8653973fe682781ce8a45b2e1d139a649
imakewebthings <caleb@imakewebthings.com>
2014-10-20 18:24:38 -0700

    View caching of browse tables

--- app/workers/document_type_count_worker.rb
+++ app/workers/document_type_count_worker.rb
@@ -6,6 +6,7 @@ class DocumentTypeCountWorker
       key = "document_type_#{category}_counts"
       val = DocumentTypeCounter.public_send "#{category}_counts"
       Rails.cache.write(key, val)
+      ActionController::Base.new.expire_fragment "#{category}_count_view"
     end
   end
 end
