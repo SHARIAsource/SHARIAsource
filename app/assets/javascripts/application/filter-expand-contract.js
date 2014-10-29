e46b5442ff234e7d750dc11bed420324a7d245c2
imakewebthings <caleb@imakewebthings.com>
2014-10-29 13:33:10 -0700

    Expand search filter blocks with selected filters

--- app/assets/javascripts/application/filter-expand-contract.js
+++ app/assets/javascripts/application/filter-expand-contract.js
@@ -1,16 +1,7 @@
 (function() {
-  $(document).on('click', '.filter-block .expand-contract', function(event) {
-    var $block = $(this).closest('.filter-block')
-    var $filterHeader = $('.filter-header')
+  var $filterHeader
 
-    if ($('.search-full-width').length) {
-      return false;
-    }
-
-    $block.toggleClass('is-contracted')
-
-    $block.find('.filter-content').slideToggle(300)
-
+  function checkExpandContractAll() {
     $filterHeader.removeClass('all-expanded all-contracted')
     if ($('.filter-block').length === $('.is-contracted').length) {
       $filterHeader.addClass('all-contracted')
@@ -18,18 +9,44 @@
     if (!$('.filter-block.is-contracted').length) {
       $filterHeader.addClass('all-expanded')
     }
+  }
+
+  $(document).on('click', '.filter-block .expand-contract', function(event) {
+    var $block = $(this).closest('.filter-block')
+
+    if ($('.search-full-width').length) {
+      return false;
+    }
+
+    $block.toggleClass('is-contracted')
+    $block.find('.filter-content').slideToggle(300)
+    checkExpandContractAll()
     event.preventDefault()
   }).on('click', '.filter-header .expand-all a', function(event) {
     var $this = $(this)
 
-    $('.filter-header').addClass('all-expanded')
+    $filterHeader.addClass('all-expanded')
     $('.filter-block.is-contracted .expand-contract').trigger('click')
     event.preventDefault()
   }).on('click', '.filter-header .contract-all a', function(event) {
     var $this = $(this)
 
-    $('.filter-header').addClass('all-contracted')
+    $filterHeader.addClass('all-contracted')
     $('.filter-block:not(.is-contracted) .expand-contract').trigger('click')
     event.preventDefault()
+  }).on('page:change', function() {
+    var checkedSelector = 'input[type="checkbox"]:checked:not(.all input)'
+    var $blocks = $('.filter-block')
+    var $checked = $blocks.find(checkedSelector)
+    var $selected = $blocks.find('option:selected')
+    var $filled = $blocks.find('input[type="text"]').filter(function() {
+      return !!$(this).val()
+    })
+    var $expand = $checked.add($selected).add($filled).closest('.filter-block')
+
+    $filterHeader = $('.filter-header')
+    $expand.removeClass('is-contracted')
+    $expand.find('.filter-content').slideToggle(300)
+    checkExpandContractAll()
   })
 }())
