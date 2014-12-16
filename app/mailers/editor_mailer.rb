b9061f9064e2c60b5e6f2c1f16031fd1e994302d
imakewebthings <caleb@imakewebthings.com>
2014-12-16 09:23:11 -0800

    Change who receives tag suggestion emails

--- app/mailers/editor_mailer.rb
+++ app/mailers/editor_mailer.rb
@@ -2,8 +2,11 @@ class EditorMailer < ActionMailer::Base
   def tag_suggestion_email(document, suggestions)
     @document = document
     @suggestions = suggestions
-    User.editors.each do |editor|
-      mail(to: editor.email, subject: "Tag Suggestions for #{document.title}")
-    end
+
+    mail(
+      to: document.contributor.email,
+      subject: "Tag Suggestions for #{document.title}",
+      cc: document.contributor.ancestors.pluck(:email)
+    )
   end
 end
