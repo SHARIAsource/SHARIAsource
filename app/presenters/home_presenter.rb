460cd7d7c81f8900d6a7a14758850b05eb7c4a12
Brian 'Phunk' Gadoury <bgadoury@endpoint.com>
2016-11-09 18:49:16 -0500

    Add Misc-driven "About" blurb to home page

--- app/presenters/home_presenter.rb
+++ app/presenters/home_presenter.rb
@@ -1,7 +1,10 @@
 class HomePresenter
-  attr_reader :recent, :popular, :featured
+  attr_reader :about, :recent, :popular, :featured
 
   def initialize
+    # NOTE: This content needs to be created with this slug in order to
+    #   populate the "about" blurb displayed on the home page.
+    @about = Misc.find_by(slug: 'about-short').try(:body).to_s
     @recent = Document.published.latest.limit(3).map do |d|
       DocumentPresenter.new d
     end
