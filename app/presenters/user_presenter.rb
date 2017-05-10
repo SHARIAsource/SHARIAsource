1573d5f4c49a5b2873fcf9c32a8b862bcdde8dff
Brian 'Phunk' Gadoury <bgadoury@endpoint.com>
2017-05-10 13:24:08 -0400

    Add basic tabbed UI on contributor biography

--- app/presenters/user_presenter.rb
+++ app/presenters/user_presenter.rb
@@ -1,5 +1,30 @@
 class UserPresenter < BasePresenter
   def name_with_collaborator
-    [name, collaborator && collaborator.name].compact.join(', ')
+    [name, collaborator.try(:name)].compact.join(', ')
   end
+
+  def biography_uses_tabs?
+    is_senior_scholar?
+  end
+
+  def biography_sections
+    # partial filename => tab text for that section
+    {
+      'about' => 'Biography',
+      'publications' => 'Publications',
+      'syllabi' => 'Syllabi',
+      'other_links' => 'Other Links',
+    }
+  end
+
+  def sections_with_content
+    # Returns a hash of section names mapped to their content, which
+    # might be nil
+    @sections_with_content ||= {
+      'about' => about,
+      'publications' => 'Publication content',
+      'syllabi' => 'Syllabi content',
+    }
+  end
+
 end
