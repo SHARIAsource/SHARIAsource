61f0af6f0b167903d9b089c0513c894678c25e33
Kamil Ciemniewski <kamil@endpoint.com>
2017-06-30 08:58:49 -0400

    Fixed teh initialization of the TinyMCE

--- app/assets/javascripts/admin-inits.js
+++ app/assets/javascripts/admin-inits.js
@@ -2,35 +2,43 @@ var $document = $(document)
 
 $document.on('page:change', function() {
   $document.foundation()
+  tinymce.remove();
 
   $('select[multiple]').chosen()
 
-  tinymce.init({
-    selector: 'textarea.wysiwyg',
-    toolbar: 'formatselect styleselect | bold italic underline strikethrough superscript | bullist numlist blockquote | link unlink | image media table | undo redo | ltr rtl',
-    plugins: 'link image table paste directionality media',
-    browser_spellcheck: true,
-    style_formats: [{
-      title: 'Footnote',
-      block: 'p',
-      classes: 'ss-footnote'
-    }],
-    menubar: false,
-    statusbar: false,
-    paste_remove_styles: true,
-    height: 250,
-    content_css: $('meta[name="tinymce-content-css"]').attr('content')
-  })
+  var initWysiwygs = function() {
+    return tinymce.init({
+      selector: 'textarea.wysiwyg',
+      toolbar: 'formatselect styleselect | bold italic underline strikethrough superscript | bullist numlist blockquote | link unlink | image media table | undo redo | ltr rtl',
+      plugins: 'link image table paste directionality media',
+      browser_spellcheck: true,
+      style_formats: [{
+        title: 'Footnote',
+        block: 'p',
+        classes: 'ss-footnote'
+      }],
+      menubar: false,
+      statusbar: false,
+      paste_remove_styles: true,
+      height: 250,
+      content_css: $('meta[name="tinymce-content-css"]').attr('content')
+    });
+  };
 
-  tinymce.init({
-    selector: 'textarea.wysiwyg-summary',
-    toolbar: 'italic link unlink | bold italic underline strikethrough superscript | bullist numlist blockquote | image media | undo redo',
-    plugins: 'link image media paste',
-    browser_spellcheck: true,
-    menubar: false,
-    statusbar: false,
-    paste_remove_styles: true,
-    height: 150,
-    content_css: $('meta[name="tinymce-content-css"]').attr('content')
-  })
+  var initSummaryWysiwygs = function() {
+    return tinymce.init({
+      selector: 'textarea.wysiwyg-summary',
+      toolbar: 'italic link unlink | bold italic underline strikethrough superscript | bullist numlist blockquote | image media | undo redo',
+      plugins: 'link image media paste',
+      browser_spellcheck: true,
+      menubar: false,
+      statusbar: false,
+      paste_remove_styles: true,
+      height: 150,
+      content_css: $('meta[name="tinymce-content-css"]').attr('content')
+    });
+  };
+
+  initWysiwygs();
+  initSummaryWysiwygs();
 })
