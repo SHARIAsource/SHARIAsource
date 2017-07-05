a8825a079f2af0af9b30e4ecd31f45cd4a4de51d
Matt Galvin <mgalvin@endpoint.com>
2017-07-05 17:30:28 -0400

    Remove EnvCheck

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,5 @@
-require_relative "../../tmp/envcheck"
 
 class ApplicationController < ActionController::Base
-  include Envcheck
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
