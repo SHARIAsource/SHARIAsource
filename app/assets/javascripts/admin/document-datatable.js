3e93319ced9983faec2c0a25f5f0981a58895b7d
Matt Galvin <mgalvin@endpoint.com>
2017-09-12 15:26:45 -0400

    Resolve bugs related to 14234, Rails update

--- app/assets/javascripts/admin/document-datatable.js
+++ app/assets/javascripts/admin/document-datatable.js
@@ -1,48 +1,52 @@
 var ready;
 ready = function() {
-    // TODO: This assumes sSearch, if present, is the first search term
-    var terms = window.location.search.replace('?', '').split('sSearch=');
-    var q = terms.length == 2 ? decodeURIComponent(terms[1]) : null;
-    //console.log('Loading search term: ' + q);
+    //prevent re-init of data table, prevent loss of init when going from 
+    //unpublished to pubslihed and vice versa
+    if(!$("#document-datatable_filter")[0]) {
 
-    var oTable = $('#document-datatable').dataTable({
-        sPaginationType: 'full_numbers',
-        search: {
-            search: q
-        },
-        bJQueryUI: true,
-        bProcessing: true,
-        bServerSide: true,
-        sAjaxSource: $('#document-datatable').data('source'),
-        "columnDefs": [{
-            "targets": -1,
-            "className": 'details-control',
-            "orderable": false,
-            "searchable": false
-        },
-        {
-            "targets": -3,
-            "className": 'reviewed'
-        }]
-    });
-
-    var search_box = $('#document-datatable_filter input');
-    search_box.unbind();
-    search_box.bind('keyup', function(e) {
-        if (e.keyCode == 13) {
-            oTable.fnFilter(this.value);
-        }
-    });
-
-    $('.document-status-nav-link').bind('click', function(e) {
-        e.preventDefault();
-        var link = $(this).attr('href');
-        var q = search_box.val();
-        if (!!q) {
-            link += '?sSearch=' + encodeURIComponent(q);
-        }
-        window.location.href = link;
-    });
+        // TODO: This assumes sSearch, if present, is the first search term
+        var terms = window.location.search.replace('?', '').split('sSearch=');
+        var q = terms.length == 2 ? decodeURIComponent(terms[1]) : null;
+        //console.log('Loading search term: ' + q);
+            var oTable = $('#document-datatable').dataTable({
+                sPaginationType: 'full_numbers',
+                search: {
+                    search: q
+                },
+                bJQueryUI: true,
+                bProcessing: true,
+                bServerSide: true,
+                sAjaxSource: $('#document-datatable').data('source'),
+                "columnDefs": [{
+                    "targets": -1,
+                    "className": 'details-control',
+                    "orderable": false,
+                    "searchable": false
+                },
+                {
+                    "targets": -3,
+                    "className": 'reviewed'
+                }]
+            });
+    
+        var search_box = $('#document-datatable_filter input');
+        search_box.unbind();
+        search_box.bind('keyup', function(e) {
+            if (e.keyCode == 13) {
+                oTable.fnFilter(this.value);
+            }
+        });
+    
+        $('.document-status-nav-link').bind('click', function(e) {
+            e.preventDefault();
+            var link = $(this).attr('href');
+            var q = search_box.val();
+            if (!!q) {
+                link += '?sSearch=' + encodeURIComponent(q);
+            }
+            window.location.href = link;
+        });
+    }
 }
 
 $(document).ready(ready);
