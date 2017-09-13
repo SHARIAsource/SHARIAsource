c005040456f4bafd9f058fb362fbf8a3622696c3
Matt Galvin <mgalvin@endpoint.com>
2017-09-13 10:13:33 -0400

    Temp. update to user_presenter as related to 14411, senior scholar tabs

--- app/presenters/user_presenter.rb
+++ app/presenters/user_presenter.rb
@@ -9,12 +9,12 @@ class UserPresenter < BasePresenter
 
   def biography_sections
     # partial filename => tab text for that section
-    {
-      'about' => 'Biography',
-      'publications' => 'Publications',
-      'syllabi' => 'Syllabi',
-      'other_links' => 'Other Links',
-    }
+    tabs = {}
+    tabs['about'] = 'Biography'
+    tabs['publications'] = 'Publications'
+    tabs['syllabi'] = 'Syllabi'
+    tabs['other_links'] = 'Other Links'
+    tabs
   end
 
   def sections_with_content
@@ -22,9 +22,10 @@ class UserPresenter < BasePresenter
     # might be nil
     @sections_with_content ||= {
       'about' => about,
-      'publications' => 'Publication content',
-      'syllabi' => 'Syllabi content',
-    }
+      'publications' => publications,
+      'syllabi' =>  Document.where(contributor_id: id, document_type_id: DocumentType.find_by_name( 'Syllabi').id),
+      'other_links' => other_links,
+    }.reject { |k, v| v.blank? }
   end
 
 end
