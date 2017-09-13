289ba9cea6ca20feb72741999e827dfd477af575
Matt Galvin <mgalvin@endpoint.com>
2017-09-13 10:22:13 -0400

    Make migration related to 14411 be compatible with Rails 5

--- db/migrate/20170727190803_add_publications_and_other_links_to_users.rb
+++ db/migrate/20170727190803_add_publications_and_other_links_to_users.rb
@@ -1,4 +1,4 @@
-class AddPublicationsAndOtherLinksToUsers < ActiveRecord::Migration
+class AddPublicationsAndOtherLinksToUsers < ActiveRecord::Migration[5.1]
   def change
     change_table :users do |t|
       t.text :publications
