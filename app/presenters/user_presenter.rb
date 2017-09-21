4755ff16680229f017608276e4edfd68dc189ef6
Matt Galvin <mgalvin@endpoint.com>
2017-09-21 08:27:49 -0400

    Resolve 14411, add senior scholar tabs

--- app/presenters/user_presenter.rb
+++ app/presenters/user_presenter.rb
@@ -17,13 +17,21 @@ class UserPresenter < BasePresenter
     tabs
   end
 
+  def get_syllabi
+    doc_type = DocumentType.find_by_name "Islamic Law Teaching"
+    syllabus = ReferenceType.find_by_name "Syllabus"
+    Document.where(contributor_id: id,
+                   reference_type_id: syllabus,
+                   document_type_id: doc_type)
+  end
+
   def sections_with_content
     # Returns a hash of section names mapped to their content, which
     # might be nil
     @sections_with_content ||= {
       'about' => about,
       'publications' => publications,
-      'syllabi' =>  Document.where(contributor_id: id, document_type_id: DocumentType.find_by_name( 'Syllabi').id),
+      'syllabi' =>  get_syllabi,
       'other_links' => other_links,
     }.reject { |k, v| v.blank? }
   end
