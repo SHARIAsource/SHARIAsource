086e07e9cb1e12728d7e5b05b69f9d4aea0a73b0
Kamil Ciemniewski <kamil@endpoint.com>
2017-09-29 06:36:53 -0400

    Added the notion of a named filter

---
+++ app/models/named_filter.rb
@@ -0,0 +1,10 @@
+class NamedFilter < ActiveRecord::Base
+  belongs_to :language, required: false
+  belongs_to :contributor, foreign_key: :user_id, class_name: 'User', required: false
+  belongs_to :topic, required: false
+  belongs_to :theme, required: false
+  belongs_to :region, required: false
+  belongs_to :era, required: false
+  belongs_to :document_type, required: false
+  belongs_to :project, required: false
+end
---
+++ spec/factories/named_filters.rb
@@ -0,0 +1,18 @@
+FactoryGirl.define do
+  factory :named_filter do
+    name "MyString"
+    q "MyString"
+    date_from "2017-09-29"
+    date_to "2017-09-29"
+    date_format "MyString"
+    language nil
+    contributor nil
+    topic nil
+    theme nil
+    region nil
+    era nil
+    document_type nil
+    sort "MyString"
+    page 1
+  end
+end
