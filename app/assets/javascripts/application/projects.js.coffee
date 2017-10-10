7872b1b479722f194f63007f96dc7efed0174457
Matt Galvin <mgalvin@endpoint.com>
2017-10-10 16:54:45 -0400

    WIP, #14784 Special Project Template

--- app/assets/javascripts/application/projects.js.coffee
+++ app/assets/javascripts/application/projects.js.coffee
@@ -2,6 +2,24 @@
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
 $(document).on 'ready page:load', ->
+  $('#all_named_search').on 'click', (event) ->
+    check_boxes = $('.additional_search')
+    i = 0
+    filter_ids = []
+    while i < check_boxes.length
+      check_boxes[i].checked = true
+      filter_ids.push check_boxes[i].value
+      i++
+    $.ajax
+     data: 
+      named_filter_id: filter_ids
+     url: '/projects/' + this.value
+     success: (res) ->
+       search_results = res.indexOf('<div class="search-results">')
+       footer_div = res.indexOf('<div class="inner-wrapper"><div class="footer">')
+       $(".search-results").replaceWith(res.substring(search_results, footer_div));
+       return
+
   $('.additional_search').on 'click', (event) ->
     check_boxes = $('.additional_search')
     checked_boxes = []
@@ -10,8 +28,6 @@ $(document).on 'ready page:load', ->
       if check_boxes[i].checked
         checked_boxes.push check_boxes[i]
       i++
-    if checked_boxes.length < 1
-      return
     filter_ids = []
     i = 0
     while i < checked_boxes.length
@@ -21,7 +37,9 @@ $(document).on 'ready page:load', ->
       $.ajax
         url: '/projects/' + this.id
         success: (res) ->
-          console.log(res)
+          search_results = res.indexOf('<div class="search-results">')
+          footer_div = res.indexOf('<div class="inner-wrapper"><div class="footer">')
+          $(".search-results").replaceWith(res.substring(search_results, footer_div));
           return
     else
       $.ajax
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -10,8 +10,9 @@ class ProjectsController < ApplicationController
 
   private
   #this function allows a  user to check multiple 'Additional Searches' on the projects page
-  def inject_ids(attribute)
-    @filters.inject([]) { |array_of_ids, filter| array_of_ids = [] if array_of_ids.nil?; array_of_ids.push filter.try(attribute).id if filter.try(attribute).present? }
+  def inject_ids(filters,attribute)
+    logger.debug "Injecting #{attribute}"
+    filters.inject([]) { |array_of_ids, filter| array_of_ids = [] if array_of_ids.nil?; array_of_ids.push filter.try(attribute).id if filter.try(attribute).present?; array_of_ids }
   end
 
   def fetch_filters
@@ -19,34 +20,38 @@ class ProjectsController < ApplicationController
     @filters = filter_ids.present? ? NamedFilter.find(filter_ids) : NamedFilter.new
     #if this method  is being hit by clicking checkboxes on projects show page it will be an array
     #if not it will just be an instance of the NamedFilter class
+    logger.debug "Document Type: #{@filters.inspect}"
+    filters = @filters
     if @filters.class == Array
       @search = Document.search do |query|
-        query.fulltext @filters.inject(nil) { |fulltext, filter_text| fulltext or filter_text.q }
+        query.all_of do
+          logger.debug "Filters: #{filters}"
+          query.fulltext filters.inject(nil) { |fulltext, filter_text| fulltext or filter_text.q }
+          query.with(:published, true)
+          query.with(:contributor_id, inject_ids(filters,'contributor')) unless inject_ids(filters,'contributor').empty?
+          query.with(:region_ids, inject_ids(filters,'region')) unless inject_ids(filters,'region').empty?
+          query.with(:language_id, inject_ids(filters,'language')) unless inject_ids(filters,'language').empty?
+          query.with(:document_type_id, inject_ids(filters,'document_type')) unless inject_ids(filters,'document_type').empty?
+          query.with(:theme_ids, inject_ids(filters,'theme')) unless inject_ids(filters,'theme').empty?
+          query.with(:topic_ids, inject_ids(filters,'topic')) unless inject_ids(filters,'topic').empty?
+          query.with(:era_ids, inject_ids(filters,'era')) unless inject_ids(filters,'era').empty?
+          query.paginate :page => 1, :per_page => 5
+        end
+      end
+      logger.debug "Search: #{@search.inspect}"
+    else
+      @search = Document.search do |query|
+        query.fulltext @filters.q
         query.with(:published, true)
-        query.with(:contributor_id, inject_ids('contributor')) if inject_ids('contributor')
-        query.with(:region_ids, inject_ids('region')) if inject_ids('region') 
-        query.with(:language_id, inject_ids('language')) if inject_ids('language')
-        query.with(:document_type_id, inject_ids('document_type')) if inject_ids('document_type')
-        query.with(:theme_ids, inject_ids('theme')) if inject_ids('theme')
-        query.with(:topic_ids, inject_ids('topic')) if inject_ids('topic')
-        query.with(:era_ids, inject_ids('era')) if inject_ids('era')
-      end
-       #temp workround since views expected only one NamedFilter
-       @filters = @filters.last
-     else
-       @search = Document.search do |query|
-         query.fulltext @filters.q
-         query.with(:published, true)
-         query.with(:contributor_id, @filters.contributor.id) if @filters.contributor.present?
-         query.with(:region_ids, @filters.region.id) if @filters.region.present?
-         query.with(:language_id, [@filters.language.id]) if @filters.language.present?
-         query.with(:document_type_id, [@filters.document_type.id]) if @filters.document_type.present?
-         query.with(:theme_ids, [@filters.theme.id]) if @filters.theme.present?
-         query.with(:topic_ids, [@filters.topic.id]) if @filters.topic.present?
-         query.with(:era_ids, [@filters.era.id]) if @filters.era.present?
-      end
-      #temp workround since views expected only one NamedFilter
-      @filters = NamedFilter.new
+        query.with(:contributor_id, @filters.contributor.id) if @filters.contributor.present?
+        query.with(:region_ids, @filters.region.id) if @filters.region.present?
+        query.with(:language_id, [@filters.language.id]) if @filters.language.present?
+        query.with(:document_type_id, [@filters.document_type.id]) if @filters.document_type.present?
+        query.with(:theme_ids, [@filters.theme.id]) if @filters.theme.present?
+        query.with(:topic_ids, [@filters.topic.id]) if @filters.topic.present?
+        query.with(:era_ids, [@filters.era.id]) if @filters.era.present?
+        query.paginate :page => 1, :per_page => 5
+     end
     end
   end
 
