c8c656278c80f702380f72655828dedac93bdfdc
Kamil Ciemniewski <kamil@endpoint.com>
2018-03-22 15:32:15 +0000

    Made part of the integration with corpusbuilder sane

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -2,4 +2,11 @@ module AdminHelper
   def admin_title(page_title)
     [page_title, 'SHARIAsource Admin'].flatten.reject(&:empty?).join ' | '
   end
+
+  def corpusbuilder_uploader
+    render partial: "admin/shared/corpusbuilder_uploader", locals: {
+      corpusbuilder_public_url: Corpusbuilder::Ruby::Api.config.public_url,
+      editor_email: 'kamil@endpoint.com'
+    }
+  end
 end
--- config/initializers/corpusbuilder.rb
+++ config/initializers/corpusbuilder.rb
@@ -1,8 +1,9 @@
 Corpusbuilder::Ruby::Api.config.api_url = "http://0.0.0.0:7946"
+Corpusbuilder::Ruby::Api.config.public_url = "http://46.shariasource.berkman.temphost.net:7946"
 Corpusbuilder::Ruby::Api.config.api_version = 1
 Corpusbuilder::Ruby::Api.config.app_id = "453aac29-386f-433a-b581-3b12912cc48e"
 Corpusbuilder::Ruby::Api.config.token = "$2a$08$xU.rPLgjgYzX6gLGZuI2N.v3e4lhevipy.9kNH4YAVrpYqHkHT3Nq"
 Corpusbuilder::Ruby::Api.config.editor_id = -> (controller) {
-  "ba67ed9d-8801-4226-8819-95901c564375"
+  "06de30e4-dd98-4cef-bf47-a43dc760a50e"
   #controller.current_user.try(:editor_id)
 }
