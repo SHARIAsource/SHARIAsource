a057a98112f379afe8cb5bbf5ebb9f5d0d8b90fd
Kamil Ciemniewski <kamil@endpoint.com>
2018-06-06 14:07:16 +0000

    Fixed the show more and less

--- app/assets/javascripts/application/metadata-truncate.js
+++ app/assets/javascripts/application/metadata-truncate.js
@@ -1,15 +1,26 @@
 (function() {
+  var max = 148;
+
   $(document).on('turbolinks:load', function() {
-    // ...
+    var metadata = $('.metadata')[0];
+    var maxChild = Math.max.apply(null, Array.from(metadata.children).map(col => Math.max.apply(null, Array.from(col.children).map(el => el.offsetHeight))));
+    if(max > maxChild) {
+      $('.meta-show-more, .meta-show-less').hide();
+    }
+    else {
+      $('.meta-column').css('max-height', max + 'px');
+    }
   }).on('click', '.meta-show-more', function(event) {
     $(event.target).parents('.metadata').css('max-height', 'none');
+    $(event.target).parents('.metadata').find('.meta-column').css('max-height', 'none');
     $(event.target).parents('.metadata').find('.meta-show-less').show();
     $(event.target).hide();
     $(document).trigger('sameheight:refresh');
     event.preventDefault()
 
   }).on('click', '.meta-show-less', function(event) {
-    $(event.target).parents('.metadata').css('max-height', '142px');
+    $(event.target).parents('.metadata').css('max-height', max + 'px');
+    $(event.target).parents('.metadata').find('.meta-column').css('max-height', max + 'px');
     $(event.target).parents('.metadata').find('.meta-show-more').show();
     $(event.target).hide();
     $(document).trigger('sameheight:refresh');
