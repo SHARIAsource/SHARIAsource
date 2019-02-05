fa070daca5ed5bfcbbd411d3b51277b9003a5980
Kamil Ciemniewski <kamil@endpoint.com>
2019-02-05 07:17:18 -0500

    Further styled the OpenITI portal

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,6 +12,6 @@ module ApplicationHelper
   end
 
   def head_title(page_title)
-    [page_title, 'Application'].flatten.reject(&:empty?).join ' | '
+    [page_title, 'OpenITI'].flatten.reject(&:empty?).join ' | '
   end
 end
