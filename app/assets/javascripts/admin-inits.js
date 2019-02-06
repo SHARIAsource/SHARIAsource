79430abe7a896167fe895735a32c339687062090
Kamil Ciemniewski <kamil@endpoint.com>
2019-02-06 06:52:12 -0500

    Added one footer item and made wysiwyg in admin include source editing

--- app/assets/javascripts/admin-inits.js
+++ app/assets/javascripts/admin-inits.js
@@ -9,8 +9,8 @@ ready = function() {
 
   tinymce.init({
     selector: 'textarea.wysiwyg',
-    toolbar: 'formatselect styleselect | bold italic underline strikethrough superscript | bullist numlist blockquote | link unlink | image media table | undo redo | ltr rtl',
-    plugins: 'link image table paste directionality media autosave',
+    toolbar: 'formatselect styleselect | bold italic underline strikethrough superscript | bullist numlist blockquote | link unlink | image media table | undo redo | ltr rtl | code',
+    plugins: 'link image table paste directionality media autosave code',
     browser_spellcheck: true,
     style_formats: [{
       title: 'Footnote',
@@ -27,8 +27,8 @@ ready = function() {
 
   tinymce.init({
     selector: 'textarea.wysiwyg-summary',
-    toolbar: 'italic link unlink | bold italic underline strikethrough superscript | bullist numlist blockquote | image media | undo redo',
-    plugins: 'link image media paste autosave',
+    toolbar: 'italic link unlink | bold italic underline strikethrough superscript | bullist numlist blockquote | image media | undo redo | code',
+    plugins: 'link image media paste autosave code',
     browser_spellcheck: true,
     menubar: false,
     autosave_restore_when_empty: true,
