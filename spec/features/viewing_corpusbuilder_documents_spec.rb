c9b318a08fd56b6d91451561288a5468295092c3
Kamil Ciemniewski <kamil@endpoint.com>
2019-11-06 06:03:45 -0500

    Made teh system pass next tests - about editing lines

--- spec/features/viewing_corpusbuilder_documents_spec.rb
+++ spec/features/viewing_corpusbuilder_documents_spec.rb
@@ -1,6 +1,57 @@
 require 'rails_helper'
 
 feature 'Viewing CorpusBuilder documents' do
+  def turn_edit_mode
+    Timeout::timeout(2*60, Timeout::Error, "Couldn't find the edit button") do
+      while page.evaluate_script('$(".corpusbuilder-viewer:nth-child(1) .corpusbuilder-button-edit").length') == 0
+        sleep 2
+      end
+    end
+
+    page.evaluate_script('$(".corpusbuilder-viewer:nth-child(1) .corpusbuilder-button-edit")[0].click()')
+
+    Timeout::timeout(2*60, Timeout::Error, "Couldn't find the edit lines") do
+      while page.evaluate_script('$(".corpusbuilder-viewer:nth-child(1) .corpusbuilder-document-line-editing").length') == 0
+        sleep 2
+      end
+    end
+  end
+
+  def begin_edit_line(number)
+    page.evaluate_script("$(\".corpusbuilder-viewer:nth-child(1) .corpusbuilder-document-line-editing:nth-child(#{number})\").click()")
+
+    Timeout::timeout(2*60, Timeout::Error, "Couldn't find the editor box") do
+      while page.evaluate_script('$(".corpusbuilder-viewer:nth-child(1) .corpusbuilder-inline-editor-shell").length') == 0
+        sleep 2
+      end
+    end
+  end
+
+  def set_input_value(selector, txt)
+    page.evaluate_script("window.___input_node = $('#{selector}')[0]")
+    page.evaluate_script("Object.getOwnPropertyDescriptor(window.___input_node.__proto__, 'value').set.call(window.___input_node, '#{txt}')")
+    page.evaluate_script('window.___input_node.dispatchEvent(new Event("input", { bubbles: true }))')
+  end
+
+  def edit_word(number, txt)
+    set_input_value(
+      ".corpusbuilder-viewer:nth-child(1) .corpusbuilder-inline-editor-input:nth-child(#{number})",
+      txt
+    )
+  end
+
+  def save_line
+    page.evaluate_script("$('.corpusbuilder-viewer:nth-child(1) .corpusbuilder-inline-editor-buttons-aside button:contains(Save)')[0].click()")
+  end
+
+  def ensure_line_contains(number, txt)
+    Timeout::timeout(2*60, Timeout::Error, "Couldn't find the '#{txt}' in line #{number}") do
+      while page.evaluate_script("$(\".corpusbuilder-viewer:nth-child(1) .corpusbuilder-document-line:nth-child(#{number})\").text().match(/#{txt}/)").nil?
+        sleep 2
+      end
+    end
+  end
+
   let :document do
     create :document,
       title: "Abhath",
@@ -25,7 +76,7 @@ feature 'Viewing CorpusBuilder documents' do
     end
   end
 
-  scenario "logging in as master branch owner user sees the Edit button" do
+  scenario "logging in as master branch owner user sees the Edit button", js: true do
     sign_in_admin
 
     visit document_path(id: document.id)
@@ -37,7 +88,7 @@ feature 'Viewing CorpusBuilder documents' do
     end
   end
 
-  scenario "logging in not as master branch owner user doesn't see the Edit button" do
+  scenario "logging in not as master branch owner user doesn't see the Edit button", js: true do
     sign_in_admin "other-admin@example.com"
 
     visit document_path(id: document.id)
@@ -47,5 +98,19 @@ feature 'Viewing CorpusBuilder documents' do
         sleep 2
       end
     end
+  end
+
+  scenario "editing lines works", js: true do
+    sign_in_admin
+
+    visit document_path(id: document.id)
+
+    turn_edit_mode
+
+    begin_edit_line 1
+    edit_word 1, "test"
+    save_line
+
+    ensure_line_contains 1, "test"
   end
 end
