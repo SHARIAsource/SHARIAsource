1280e476c9b0ae83e0b163792fe76ec3a49f88c8
Kamil Ciemniewski <kamil@endpoint.com>
2020-04-09 07:26:56 +0000

    Made the top search area break off at one level

--- app/assets/javascripts/application/same_height.js
+++ app/assets/javascripts/application/same_height.js
@@ -4,12 +4,22 @@
   function sameHeight() {
     $('.js-same-height-container').each(function() {
       var $items = $(this).find('.js-same-height')
+
       $items.css('height', 'auto')
-      var heights = $.map($items.get(), function(item) {
-        return $(item).height()
-      })
-      var maxHeight = Math.max.apply(null, heights)
-      $items.height(maxHeight)
+
+      if($(this).find('.same-height-leader').length === 0) {
+        var heights = $.map($items.get(), function(item) {
+          return $(item).height()
+        })
+
+        var maxHeight = Math.max.apply(null, heights)
+        $items.height(maxHeight)
+      }
+      else {
+        $items.height(
+          $(this).find('.same-height-leader').height()
+        )
+      }
     })
   }
 
