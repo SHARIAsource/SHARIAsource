9b165a76784df7630d26798f621951a0d37b1a77
Kamil Ciemniewski <kamil@endpoint.com>
2020-04-22 15:35:56 +0000

    Fixed project persistence due to slug uniqueness

--- app/controllers/admin/projects_controller.rb
+++ app/controllers/admin/projects_controller.rb
@@ -10,30 +10,21 @@ class Admin::ProjectsController < AdminController
 
   def create
     @project = Project.new permitted_params
-    begin
-      if @project.save
-        flash[:notice] = 'New project created successfully'
-        redirect_to edit_admin_project_path @project
-      else
-        flash[:error] = @project.errors.full_messages.to_sentence
-        render :new
-      end
-    rescue ActiveRecord::RecordNotUnique
-      flash[:error] = "Project slug should be unique"
+
+    if @project.save
+      flash[:notice] = 'New project created successfully'
+      redirect_to edit_admin_project_path @project
+    else
+      flash[:error] = @project.errors.full_messages.to_sentence
       render :new
     end
   end
 
   def update
-    begin
-      if @project.update_attributes permitted_params
-        redirect_to edit_admin_project_path @project, notice: 'Project updated successfully'
-      else
-        flash[:error] = @project.errors.full_messages.to_sentence
-        render :edit
-      end
-    rescue PG::UniqueViolation
-      flash[:error] = "Project slug should be unique"
+    if @project.update_attributes permitted_params
+      redirect_to edit_admin_project_path @project, notice: 'Project updated successfully'
+    else
+      flash[:error] = @project.errors.full_messages.to_sentence
       render :edit
     end
   end
@@ -54,7 +45,23 @@ class Admin::ProjectsController < AdminController
   private
 
   def permitted_params
-    params.require(:project).permit(:name, :slug, :description, :photo, :scale_photo, :published, :projects_users_attributes => [:id, :sort_order, :user_id, :project_id, :project_role, :external_collaborator], user_ids: [])
+    params.require(:project).permit(
+      :name,
+      :slug,
+      :description,
+      :photo,
+      :scale_photo,
+      :published,
+      :projects_users_attributes => [
+        :id,
+        :sort_order,
+        :user_id,
+        :project_id,
+        :project_role,
+        :external_collaborator
+      ],
+      user_ids: []
+    )
   end
 
   def fetch_project
