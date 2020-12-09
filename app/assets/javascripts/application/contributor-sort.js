fd65226b690567fe0145a959b750240ecbf07c77
Cole Crawford <cole_crawford@harvard.edu>
2020-12-08 19:05:36 -0500

    Use turbolinks with contributors table

--- app/assets/javascripts/application/contributor-sort.js
+++ app/assets/javascripts/application/contributor-sort.js
@@ -1,4 +1,4 @@
-$(document).ready( function () {
+document.addEventListener("turbolinks:load", function() {
     $('#contributors-table').DataTable({
         paging: false,
         searching: false,
